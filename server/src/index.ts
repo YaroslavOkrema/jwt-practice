b3f2ef6c928722ac5a6528e76c129f0614e44394
YaroslavOkrema <yaroslav.okrema@gmail.com>
2026-08-14 02:00:32 +0300

    feat: add health check endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok"
+    });
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
